76946846af4fe7cd219c3d9152c0f053bab6fb94
kellanbail <122988698+kellanbail@users.noreply.github.com>
2023-01-18 13:30:52 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (hand == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # # . . .
+            # # # # #
+            . . # # #
+            # # . . .
+            # # . . .
+            `)
+    }
+})
